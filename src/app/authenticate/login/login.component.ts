a4cd32cfbf7ad7b9879edce4660ac088d5ed9458
ravi53529 <galinki.ravi333@gmail.com>
2023-08-19 16:56:49 +0530

    added login form

--- src/app/authenticate/login/login.component.ts
+++ src/app/authenticate/login/login.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+ public password:string='';
+ public passwordtype='password';
+ public seePasswordStatus:boolean=true;
+ public  login={
+  email:'',
+  password:''
+} 
+ constructor(){
+  
+ }
+  // to see password
+  public seePassword():void{
+   if(this.login){
+    this.passwordtype='text';
+    this.seePasswordStatus=false
+   }
+  }
+  public hidePassword():void{
+    if(this.login){
+      this.passwordtype='password';
+      this.seePasswordStatus=true
+    }
+  }
+  public onSubmit(loginForm:any):void{
+    if(this.login){
+     console.log(loginForm)
+    }
+  }
 }
